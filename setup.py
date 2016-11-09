b6a26647ed96ca28846f35b28af9aec697d3dfdc
Walter Doekes <walter+github@wjd.nu>
2016-11-09 16:23:09 +0100

    version: Bump version to 0.2.2.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name='proxtop',
-    version='0.2.1',
+    version='0.2.2',
     scripts=['proxtop'],
     data_files=[('', ['LICENSE.txt', 'README.rst'])],
     description='Proxmox resource monitor',
